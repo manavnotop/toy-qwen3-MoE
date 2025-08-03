fe82e6cd35bbe26a088eeb61ec4a2596203abeb8
manavnotop <manuagarwal1443@gmail.com>
2025-08-03 17:26:08 +0530

    Add: MoE and fix typos

--- main.py
+++ main.py
@@ -1,13 +1,81 @@
 import torch
 import torch.nn as nn
 
+class FeedForward(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+    self.fc1 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"], bias=False)
+    self.fc2 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+    self.fc3 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"], bias=False)
+
+  def forward(self, x):
+    x_fc1 = self.fc1(x)
+    x_fc2 = self.fc2(x)
+    x = nn.functional.silu(x_fc1) * x_fc2
+    return self.fc3(x)
+  
+class MoEFeedForward(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+    self.num_experts_per_tok = cfg["num_experts_per_tok"]
+    self.num_experts = cfg["num_experts"]
+    self.gate = nn.Linear(cfg["emb_dim"], cfg["num_experts"], bias=False, dtype=cfg["dtype"])
+
+    # meta device to reduce memory pressure when initializing the model before loading weights
+    meta_device = torch.device("meta")
+    self.fc1 = nn.ModuleList([
+      nn.Linear(
+        cfg["emb_dim"], cfg["moe_intermediate_size"],
+        bias=False, dtype=cfg["dtype"], device=meta_device)
+      for _ in range(cfg["num_experts"])]
+    )
+    self.fc2 = nn.ModuleList([
+      nn.Linear(
+        cfg["emb_dim"], cfg["moe_intermediate_size"],
+        bias=False, dtype=cfg["dtype"], device=meta_device
+        )
+      for _ in range(cfg["num_experts"])]
+    )
+    self.fc3 = nn.ModuleList([
+      nn.Linear(
+        cfg["moe_intermediate_size"], cfg["emb_dim"],
+        bias=False, dtype=cfg["dtype"], device=meta_device
+        )
+      for _ in range(cfg["num_experts"])]
+    )
+
+  def forward(self, x):
+    b, seq_len, embed_dim = x.shape
+    scores = self.gate(x)  # (b, seq_len, num_experts)
+    topk_scores, topk_indices = torch.topk(scores, self.num_experts_per_tok, dim=-1)
+    topk_probs = torch.softmax(topk_scores, dim=-1)
+    
+    expert_outputs = []
+    for e in range(self.num_experts):
+      hidden = torch.nn.functional.silu(self.fc1[e](x)) * self.fc2[e](x)
+      out = self.fc3[e](hidden)
+      expert_outputs.append(out.unsqueeze(-2))
+    expert_outputs = torch.cat(expert_outputs, dim=-2)  # (b, t, num_experts, emb_dim)
+
+    gating_probs = torch.zeros_like(scores)
+
+    for i in range(self.num_experts_per_tok):
+      indices = topk_indices[..., i:i+1]
+      prob = topk_probs[..., i:i+1]
+      gating_probs.scatter_(dim=-1, index=indices, src=prob)
+    gating_probs = gating_probs.unsqueeze(-1)  # (b, t, num_experts, 1)
+    
+    # Weighted sum over experts
+    y = (gating_probs * expert_outputs).sum(dim=-2)
+    return y
+
 class RMSNorm(nn.Module):
   def __init__(self, emb_dim, eps=1e-6, bias=False, qwen3_compatible=True):
     super().__init__()
     self.eps = eps
     self.qwen3_compatible = qwen3_compatible
     self.scale = nn.Parameter(torch.ones(emb_dim))
-    self.shift = nn.Parameter(torch.zero(emb_dim)) if bias else None
+    self.shift = nn.Parameter(torch.zeros(emb_dim)) if bias else None
 
   def forward(self, x):
     input_type = x.dtype
@@ -16,7 +84,7 @@ class RMSNorm(nn.Module):
       x = x.to(torch.float32)
     
     #next 3 lines are main maths, rest is optimisation for better calcultion
-    mean_square = x.pow(2).mean(dim=-1, keep_dim=True)
+    mean_square = x.pow(2).mean(dim=-1, keepdim=True)
     norm_x = x * torch.rsqrt(mean_square + self.eps)
     norm_x = norm_x * self.scale
 
@@ -36,10 +104,10 @@ def compute_rope_params(head_dim, theta_base=10000, context_length=4096, dtype=t
 
   position = torch.arange(context_length, dtype=dtype)
 
-  freqeuncies = inv_freq.unsqueeze(0)
+  frequencies = inv_freq.unsqueeze(0)
   position = position.unsqueeze(1)
 
-  angles = position * freqeuncies
+  angles = position * frequencies
   angles = torch.cat([angles, angles], dim=1)
 
   cos = torch.cos(angles)
@@ -55,8 +123,8 @@ def apply_rope(x, cos, sin):
   x1 = x[..., : head_dim // 2]
   x2 = x [..., head_dim //2 :]
 
-  cos = cos[:seq_len, :].unsqeeze(0).unsqueeze(0)
-  sin = sin[:seq_len, :].unsqeeze(0).unsqueeze(0)
+  cos = cos[:seq_len, :].unsqueeze(0).unsqueeze(0)
+  sin = sin[:seq_len, :].unsqueeze(0).unsqueeze(0)
 
   rotated = torch.cat([-x2, x1], dim=-1)
   x_rotated = x * cos + rotated * sin
@@ -80,7 +148,7 @@ class GroupedQueryAttention(nn.Module):
     self.d_out = num_heads * head_dim
 
     self.W_query = nn.Linear(d_in, self.d_out, bias=False, dtype=dtype)
-    self.W_key = nn. Linear(d_in, num_kv_groups * head_dim, bias=False, dtype=dtype)
+    self.W_key = nn.Linear(d_in, num_kv_groups * head_dim, bias=False, dtype=dtype)
     self.W_value = nn.Linear(d_in, num_kv_groups * head_dim, bias=False, dtype=dtype)
 
     self.out_proj = nn.Linear(self.d_out, d_in, bias=False, dtype=dtype)
@@ -92,7 +160,7 @@ class GroupedQueryAttention(nn.Module):
       self.q_norm = self.k_norm = None
 
   def forward(self, x, mask, cos, sin):
-    b, num_tokens = x.shape
+    b, num_tokens, _ = x.shape
 
     queries = self.W_query(x)
     keys = self.W_key(x)
@@ -170,7 +238,7 @@ class Qwen3MoE(nn.Module):
     self.final_norm = RMSNorm(cfg["emb_dim"])
     self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False, dtype=cfg["dtype"])
 
-    # Reusuable utilities
+    # Reusable utilities
     if cfg["head_dim"] is None:
       head_dim = cfg["emb_dim"] // cfg["n_heads"]
     else:
